7dd2af9c81d12d0e85d2696152f385eff504f54a
Xiong Moua <xiongmoua@hotmail.com>
2019-02-07 00:10:45 -0600

    added api functionality

---
+++ database.js
@@ -0,0 +1,53 @@
+
+const STORE = {
+  cpus: {
+    'Select a CPU': 'none',
+    'Intel i3-8100': '6162970',
+    'Intel i5-8400': '6163019',
+    'Intel i7-8700': '6162975',
+    'AMD Ryzen 5 2600': '6234508',
+    'AMD Ryzen 7 2700': '6234511'
+  },
+  mobos: {
+    'Select a Motherboard': 'none',
+    'ASUS-ROG STRIX H370-F GAMING':'6239836',
+    'ASUS-TUF Z390-PLUS GAMING': '6311233',
+    'ASUS-ROG MAXIMUS XI HERO': '6306415',
+    'ASUS-ROG STRIX B450-F GAMING': '6311234',
+    'ASUS-TUF X470-PLUS GAMING': '6306007',
+    'ASUS-ROG CROSSHAIR VII HERO': '6311236'
+  },
+  gpus: {
+    'Select a Graphics Card': 'none',
+    'EVGA - GeForce RTX 2060 XC Ultra Gaming 6GB GDDR6': '6321301',
+    'EVGA - GeForce RTX 2070 XC Ultra Gaming 8GB GDDR6': '6290661',
+    'EVGA - GeForce RTX 2080 XC GAMING 8GB GDDR6': '6290681',
+    'EVGA - GeForce RTX 2080 Ti XC Ultra Gaming 11GB GDDR6': '6290657',
+    'XFX - AMD Radeon RX 560 4GB GDDR5': '6183042',
+    'XFX - AMD Radeon RX 570 RS Black Edition 8GB GDDR5': '6202343',
+    'XFX - AMD Radeon RX 580 GTS Black Core Edition 8GB GDDR5': '6217300',
+    'XFX - AMD Radeon RX Vega 56 8GB HBM2': '6183041'
+  },
+  ssds: {
+    'Samsung - 860 EVO 250GB': '6178653',
+    'Samsung - 860 EVO 500GB': '6178650',
+    'Samsung - 860 EVO 1TB': '6178649'
+  },
+  psus: {
+    'CORSAIR - CX-M Series 550W': '5845208',
+    'CORSAIR - CX-M Series 650W': '5845214',
+    'CORSAIR - CX-M Series 750W': '8324202',
+    'CORSAIR - RMx Series 850W': '6229601'
+  },
+  ram: {
+    'CORSAIR - Vengeance LPX 8GB (2PK 4GB) 2.4GHz DDR4 ': '6256217',
+    'CORSAIR - VENGEANCE LPX 16GB (2PK 8GB) 2.4GHz DDR4 ': '5822470',
+    'CORSAIR - Vengeance LPX 32GB (2PK x 16GB) 2.6 GHz DDR4': '4895510'  
+  },
+  cases: {
+    'CORSAIR - Carbide Series ATX Mid-Tower Case - Black/Red': '6180328',
+    'CORSAIR - Carbide Series SPEC-DELTA RGB ATX Mid-Tower Case - Black': '6323389',
+    'CORSAIR - Crystal Series 460X Compact ATX Mid-Tower Case - Black': '5832204',
+    'CORSAIR - Crystal Series 570X RGB ATX Mid-Tower Case - Black': '5774600',
+  },
+}
---
+++ index.js
@@ -0,0 +1,202 @@
+'use strict'
+
+//https:www.googleapis.com/youtube/v3/search?q=query&part=snippet&masResults=3&key=ytkey
+//responseJson.items.thumbnail.default or medium or high.url => thumbnail
+//responseJson.items.id.videoId => https://www.youtube.com/embed/videoId
+const ytkey = 'AIzaSyA33xkrHuTLHRDvibawSxjrekL4GIX66TI';
+
+const ytUrl = 'https://www.googleapis.com/youtube/v3/search';
+
+//https://api.bestbuy.com/v1/products/sku.json?apiKey=bbkey
+//responseJson.regularPrice => price
+const bbkey = 'tRN7x3ANSvBOc14AAe9lhXb3';
+
+const bbUrl = 'https://api.bestbuy.com/v1/products/';
+
+function startBuild(event) {
+  $('#js-start').submit(event => {
+    event.preventDefault();
+    $('.js-main-container').empty();
+    $('.js-render').removeClass('hidden');
+    populateOptions();
+  });
+}
+
+function populateOptions() {
+  populateCpu();
+  populateMobo();
+}
+
+function populateCpu() {
+  const cpuName = Object.keys(STORE.cpus);
+  for (let i = 0; i < cpuName.length; i++) {
+    let option = document.createElement('option');
+    option.setAttribute('value', cpuName[i]);
+    option.text = cpuName[i];
+    let selectCpu = document.getElementById('js-cpu');
+    selectCpu.appendChild(option);
+  };
+}
+
+function populateMobo() {
+  const moboName = Object.keys(STORE.mobos);
+  for (let i = 0; i < moboName.length; i++) {
+    let option = document.createElement('option');
+    option.setAttribute('value', moboName[i]);
+    option.text = moboName[i];
+    let selectMobo = document.getElementById('js-mobo');
+    selectMobo.appendChild(option);
+  };
+}
+
+function onSelectCpu(selection) {
+  let query = selection.value;
+  getCpuYoutube(query);
+  getCpuBestBuyPrice(STORE.cpus[query]);
+}
+
+function onSelectMobo(selection) {
+  let query = selection.value;
+  getMoboYoutube(query);
+  getMoboBestBuyPrice(STORE.mobos[query]);
+}
+
+function getCpuYoutube(query) {
+  const params = {
+    key: ytkey,
+    q: query + 'review',
+    part: 'snippet',
+    maxResults: 3
+  };
+  const queryString = formatQueryParams(params)
+  const url = ytUrl + '?' + queryString;
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => renderCpuYoutube(responseJson))
+    .catch(err => {
+      $('#js-render-cpu').text(`Something went wrong: ${err.message}`);
+    });
+}
+
+function getMoboYoutube(query) {
+  const params = {
+    key: ytkey,
+    q: query + 'review',
+    part: 'snippet',
+    maxResults: 3
+  };
+  const queryString = formatQueryParams(params)
+  const url = ytUrl + '?' + queryString;
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => renderMoboYoutube(responseJson))
+    .catch(err => {
+      $('#js-render-mobo').text(`Something went wrong: ${err.message}`);
+    });
+}
+
+function formatQueryParams(params) {
+  const queryItems = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+  return queryItems.join('&');
+}
+
+function renderCpuYoutube(responseJson) {
+  // console.log(JSON.stringify((responseJson));
+  $('#js-render-cpu').html(`<a class="myBtn" href="https://www.youtube.com/embed/${responseJson.items[0].id.videoId}">
+  <img src="${responseJson.items[0].snippet.thumbnails['default'].url}"></a>
+  <a class="myBtn" href="https://www.youtube.com/embed/${responseJson.items[1].id.videoId}">
+  <img src="${responseJson.items[1].snippet.thumbnails['default'].url}"></a>
+  <a class="myBtn" href="https://www.youtube.com/embed/${responseJson.items[2].id.videoId}">
+  <img src="${responseJson.items[2].snippet.thumbnails['default'].url}"></a>`);
+  renderModal();
+}
+
+function renderMoboYoutube(responseJson) {
+  // console.log(JSON.stringify((responseJson));
+  $('#js-render-mobo').html(`<a class="myBtn" href="https://www.youtube.com/embed/${responseJson.items[0].id.videoId}">
+  <img src="${responseJson.items[0].snippet.thumbnails['default'].url}"></a>
+  <a class="myBtn" href="https://www.youtube.com/embed/${responseJson.items[1].id.videoId}">
+  <img src="${responseJson.items[1].snippet.thumbnails['default'].url}"></a>
+  <a class="myBtn" href="https://www.youtube.com/embed/${responseJson.items[2].id.videoId}">
+  <img src="${responseJson.items[2].snippet.thumbnails['default'].url}"></a>`);
+  renderModal();
+}
+
+function renderModal() {
+  $('.myBtn').on('click', function (event) {
+    event.preventDefault();
+    $('.modal').removeClass('hidden');
+    const link = $(this).attr('href');
+    $('.modal-content').html(`<span class="close">&times;</span><iframe width="100%" height="95%" src="${link}"></iframe>`);
+    closeModal();
+  });
+}
+
+function closeModal() {
+  $('.close').on('click', function(event) {
+    event.preventDefault();
+    $('.modal').addClass('hidden');
+  });
+  // $(document).on('click', function(event) {
+  //   if (!$(event.target).closest('.modal').length) {
+  //     $('body').find('.modal').addClass('hidden');
+  //   };
+  // });
+}
+
+function getCpuBestBuyPrice(sku) {
+  const apiKey = bbkey
+  const url = bbUrl + sku + '.json' + '?apiKey=' + apiKey;
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => renderCpuBestBuyPrice(responseJson))
+    .catch(err => {
+      $('#js-render-cpu-price').text(`Something went wrong: ${err.message}`);
+    });
+}
+
+function getMoboBestBuyPrice(sku) {
+  const apiKey = bbkey
+  const url = bbUrl + sku + '.json' + '?apiKey=' + apiKey;
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => renderMoboBestBuyPrice(responseJson))
+    .catch(err => {
+      $('#js-render-mobo-price').text(`Something went wrong: ${err.message}`);
+    });
+}
+
+function renderCpuBestBuyPrice(responseJson) {
+  $('#js-render-cpu-price').html(`<span>Price in USD: $${responseJson.regularPrice}</span>`);
+}
+
+function renderMoboBestBuyPrice(responseJson) {
+  $('#js-render-mobo-price').html(`<span>Price in USD: $${responseJson.regularPrice}</span>`);
+}
+
+function onStart() {
+  startBuild();
+}
+
+$(onStart);
