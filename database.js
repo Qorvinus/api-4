22c2cfc1356cb112b3d6cb8cf4a77d850f52b61d
Xiong Moua <xiongmoua@hotmail.com>
2019-03-09 17:51:58 -0600

    added styling, refactored js

--- database.js
+++ database.js
@@ -1,15 +1,15 @@
 
 const STORE = {
-  cpus: {
-    'Select a CPU': 'none',
+  cpu: {
+    'Select a CPU': '',
     'Intel i3-8100': '6162970',
     'Intel i5-8400': '6163019',
     'Intel i7-8700': '6162975',
     'AMD Ryzen 5 2600': '6234508',
     'AMD Ryzen 7 2700': '6234511'
   },
-  mobos: {
-    'Select a Motherboard': 'none',
+  mobo: {
+    'Select a Motherboard': '',
     'ASUS-ROG STRIX H370-F GAMING':'6239836',
     'ASUS-TUF Z390-PLUS GAMING': '6311233',
     'ASUS-ROG MAXIMUS XI HERO': '6306415',
@@ -17,8 +17,8 @@ const STORE = {
     'ASUS-TUF X470-PLUS GAMING': '6306007',
     'ASUS-ROG CROSSHAIR VII HERO': '6311236'
   },
-  gpus: {
-    'Select a Graphics Card': 'none',
+  gpu: {
+    'Select a Graphics Card': '',
     'EVGA - GeForce RTX 2060 XC Ultra Gaming 6GB GDDR6': '6321301',
     'EVGA - GeForce RTX 2070 XC Ultra Gaming 8GB GDDR6': '6290661',
     'EVGA - GeForce RTX 2080 XC GAMING 8GB GDDR6': '6290681',
@@ -28,23 +28,27 @@ const STORE = {
     'XFX - AMD Radeon RX 580 GTS Black Core Edition 8GB GDDR5': '6217300',
     'XFX - AMD Radeon RX Vega 56 8GB HBM2': '6183041'
   },
-  ssds: {
+  ssd: {
+    'Select a Storage SSD': '',
     'Samsung - 860 EVO 250GB': '6178653',
     'Samsung - 860 EVO 500GB': '6178650',
     'Samsung - 860 EVO 1TB': '6178649'
   },
-  psus: {
+  psu: {
+    'Select a Power Supply': '',
     'CORSAIR - CX-M Series 550W': '5845208',
     'CORSAIR - CX-M Series 650W': '5845214',
     'CORSAIR - CX-M Series 750W': '8324202',
     'CORSAIR - RMx Series 850W': '6229601'
   },
   ram: {
-    'CORSAIR - Vengeance LPX 8GB (2PK 4GB) 2.4GHz DDR4 ': '6256217',
-    'CORSAIR - VENGEANCE LPX 16GB (2PK 8GB) 2.4GHz DDR4 ': '5822470',
-    'CORSAIR - Vengeance LPX 32GB (2PK x 16GB) 2.6 GHz DDR4': '4895510'  
+    'Select a RAM': '',
+    'CORSAIR - Vengeance LPX 8GB DDR4': '6256217',
+    'CORSAIR - VENGEANCE LPX 16GB DDR4': '5822470',
+    'CORSAIR - Vengeance LPX 32GB DDR4': '4895510'  
   },
   cases: {
+    'Select a Case': '',
     'CORSAIR - Carbide Series ATX Mid-Tower Case - Black/Red': '6180328',
     'CORSAIR - Carbide Series SPEC-DELTA RGB ATX Mid-Tower Case - Black': '6323389',
     'CORSAIR - Crystal Series 460X Compact ATX Mid-Tower Case - Black': '5832204',
--- index.js
+++ index.js
@@ -18,53 +18,112 @@ function startBuild(event) {
     event.preventDefault();
     $('.js-main-container').empty();
     $('.js-render').removeClass('hidden');
+    onIntelSelection();
+    onAmdSelection();
     populateOptions();
+    listenSelection();
+  });
+}
+
+function listenSelection() {
+  Object.keys(STORE).forEach(type => {
+    onChange(type);
+  });
+}
+
+function onChange(type) {
+  const name = Object.keys(STORE[type]);
+  for (let i = 0; i < name.length; i++);
+  $(`#js-${type}`).change(function() {
+    const select = document.getElementById(`js-${type}`);
+    const query = select.options[select.selectedIndex].text;
+    const sku = STORE[type][query];
+    onSelection(query, sku, type);
   });
 }
 
 function populateOptions() {
-  populateCpu();
-  populateMobo();
+  Object.keys(STORE).forEach(type => {
+    populateItem(type);
+  });
+  onDefaultCpu();
 }
 
-function populateCpu() {
-  const cpuName = Object.keys(STORE.cpus);
-  for (let i = 0; i < cpuName.length; i++) {
-    let option = document.createElement('option');
-    option.setAttribute('value', cpuName[i]);
-    option.text = cpuName[i];
-    let selectCpu = document.getElementById('js-cpu');
-    selectCpu.appendChild(option);
+function populateItem(type) {
+  const name = Object.keys(STORE[type]);
+  for (let i = 0; i < name.length; i++) {
+    const option = document.createElement('option');
+    option.setAttribute('value', name[i]);
+    option.text = name[i];
+    const select = document.getElementById(`js-${type}`);
+    select.appendChild(option);
   };
 }
 
-function populateMobo() {
-  const moboName = Object.keys(STORE.mobos);
-  for (let i = 0; i < moboName.length; i++) {
-    let option = document.createElement('option');
-    option.setAttribute('value', moboName[i]);
-    option.text = moboName[i];
-    let selectMobo = document.getElementById('js-mobo');
-    selectMobo.appendChild(option);
-  };
+function onDefaultCpu() {
+    $("#js-mobo option[value = 'ASUS-ROG STRIX B450-F GAMING']").prop('disabled',true);
+    $("#js-mobo option[value = 'ASUS-TUF X470-PLUS GAMING']").prop('disabled',true);
+    $("#js-mobo option[value = 'ASUS-ROG CROSSHAIR VII HERO']").prop('disabled',true);
+    // disable these
+    $("#js-mobo option[value = 'ASUS-ROG STRIX H370-F GAMING']").prop('disabled',false);
+    $("#js-mobo option[value = 'ASUS-TUF Z390-PLUS GAMING']").prop('disabled',false);
+    $("#js-mobo option[value = 'ASUS-ROG MAXIMUS XI HERO']").prop('disabled',false);
+    //cpus
+    $("#js-cpu option[value = 'Intel i3-8100']").prop('disabled',false);
+    $("#js-cpu option[value = 'Intel i5-8400']").prop('disabled',false);
+    $("#js-cpu option[value = 'Intel i7-8700']").prop('disabled',false);
+    // disable these
+    $("#js-cpu option[value = 'AMD Ryzen 5 2600']").prop('disabled',true);
+    $("#js-cpu option[value = 'AMD Ryzen 7 2700']").prop('disabled',true);
 }
 
-function onSelectCpu(selection) {
-  let query = selection.value;
-  getCpuYoutube(query);
-  getCpuBestBuyPrice(STORE.cpus[query]);
+function onIntelSelection() {
+  $('#intel').on('click', function() {
+    $("#js-mobo option[value = 'ASUS-ROG STRIX B450-F GAMING']").prop('disabled',true);
+    $("#js-mobo option[value = 'ASUS-TUF X470-PLUS GAMING']").prop('disabled',true);
+    $("#js-mobo option[value = 'ASUS-ROG CROSSHAIR VII HERO']").prop('disabled',true);
+    // disable these
+    $("#js-mobo option[value = 'ASUS-ROG STRIX H370-F GAMING']").prop('disabled',false);
+    $("#js-mobo option[value = 'ASUS-TUF Z390-PLUS GAMING']").prop('disabled',false);
+    $("#js-mobo option[value = 'ASUS-ROG MAXIMUS XI HERO']").prop('disabled',false);
+    //cpus
+    $("#js-cpu option[value = 'Intel i3-8100']").prop('disabled',false);
+    $("#js-cpu option[value = 'Intel i5-8400']").prop('disabled',false);
+    $("#js-cpu option[value = 'Intel i7-8700']").prop('disabled',false);
+    // disable these
+    $("#js-cpu option[value = 'AMD Ryzen 5 2600']").prop('disabled',true);
+    $("#js-cpu option[value = 'AMD Ryzen 7 2700']").prop('disabled',true);
+  });
 }
 
-function onSelectMobo(selection) {
-  let query = selection.value;
-  getMoboYoutube(query);
-  getMoboBestBuyPrice(STORE.mobos[query]);
+function onAmdSelection() {
+  $('#amd').on('click', function () {
+    $("#js-mobo option[value = 'ASUS-ROG STRIX B450-F GAMING']").prop('disabled',false);
+    $("#js-mobo option[value = 'ASUS-TUF X470-PLUS GAMING']").prop('disabled',false);
+    $("#js-mobo option[value = 'ASUS-ROG CROSSHAIR VII HERO']").prop('disabled',false);
+    // enable these
+    $("#js-mobo option[value = 'ASUS-ROG STRIX H370-F GAMING']").prop('disabled',true);
+    $("#js-mobo option[value = 'ASUS-TUF Z390-PLUS GAMING']").prop('disabled',true);
+    $("#js-mobo option[value = 'ASUS-ROG MAXIMUS XI HERO']").prop('disabled',true);
+    //cpus
+    $("#js-cpu option[value = 'Intel i3-8100']").prop('disabled',true);
+    $("#js-cpu option[value = 'Intel i5-8400']").prop('disabled',true);
+    $("#js-cpu option[value = 'Intel i7-8700']").prop('disabled',true);
+    // disable these
+    $("#js-cpu option[value = 'AMD Ryzen 5 2600']").prop('disabled',false);
+    $("#js-cpu option[value = 'AMD Ryzen 7 2700']").prop('disabled',false);
+  });
 }
 
-function getCpuYoutube(query) {
+function onSelection(query, sku, type) {
+  getYoutube(query, type);
+  getBestBuyPrice(sku, type);
+}
+
+function getYoutube(query, type) {
   const params = {
     key: ytkey,
-    q: query + 'review',
+    q: query,
     part: 'snippet',
     maxResults: 3
   };
@@ -77,31 +136,9 @@ function getCpuYoutube(query) {
       }
       throw new Error(response.statusText);
     })
-    .then(responseJson => renderCpuYoutube(responseJson))
+    .then(responseJson => renderYoutube(responseJson, type))
     .catch(err => {
-      $('#js-render-cpu').text(`Something went wrong: ${err.message}`);
-    });
-}
-
-function getMoboYoutube(query) {
-  const params = {
-    key: ytkey,
-    q: query + 'review',
-    part: 'snippet',
-    maxResults: 3
-  };
-  const queryString = formatQueryParams(params)
-  const url = ytUrl + '?' + queryString;
-  fetch(url)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      throw new Error(response.statusText);
-    })
-    .then(responseJson => renderMoboYoutube(responseJson))
-    .catch(err => {
-      $('#js-render-mobo').text(`Something went wrong: ${err.message}`);
+      $(`#js-render-${type}`).text(`Something went wrong: ${err.message}`);
     });
 }
 
@@ -111,20 +148,8 @@ function formatQueryParams(params) {
   return queryItems.join('&');
 }
 
-function renderCpuYoutube(responseJson) {
-  // console.log(JSON.stringify((responseJson));
-  $('#js-render-cpu').html(`<a class="myBtn" href="https://www.youtube.com/embed/${responseJson.items[0].id.videoId}">
-  <img src="${responseJson.items[0].snippet.thumbnails['default'].url}"></a>
-  <a class="myBtn" href="https://www.youtube.com/embed/${responseJson.items[1].id.videoId}">
-  <img src="${responseJson.items[1].snippet.thumbnails['default'].url}"></a>
-  <a class="myBtn" href="https://www.youtube.com/embed/${responseJson.items[2].id.videoId}">
-  <img src="${responseJson.items[2].snippet.thumbnails['default'].url}"></a>`);
-  renderModal();
-}
-
-function renderMoboYoutube(responseJson) {
-  // console.log(JSON.stringify((responseJson));
-  $('#js-render-mobo').html(`<a class="myBtn" href="https://www.youtube.com/embed/${responseJson.items[0].id.videoId}">
+function renderYoutube(responseJson, type) {
+  $(`#js-render-${type}`).html(`<a class="myBtn" href="https://www.youtube.com/embed/${responseJson.items[0].id.videoId}">
   <img src="${responseJson.items[0].snippet.thumbnails['default'].url}"></a>
   <a class="myBtn" href="https://www.youtube.com/embed/${responseJson.items[1].id.videoId}">
   <img src="${responseJson.items[1].snippet.thumbnails['default'].url}"></a>
@@ -134,11 +159,12 @@ function renderMoboYoutube(responseJson) {
 }
 
 function renderModal() {
-  $('.myBtn').on('click', function (event) {
+  $('.myBtn').on('click', function(event) {
     event.preventDefault();
-    $('.modal').removeClass('hidden');
+    $('#myModal').removeClass('hidden');
     const link = $(this).attr('href');
     $('.modal-content').html(`<span class="close">&times;</span><iframe width="100%" height="95%" src="${link}"></iframe>`);
+    console.log('modal open');
     closeModal();
   });
 }
@@ -146,7 +172,8 @@ function renderModal() {
 function closeModal() {
   $('.close').on('click', function(event) {
     event.preventDefault();
-    $('.modal').addClass('hidden');
+    $('#myModal').addClass('hidden');
+    stopModalVideo();
   });
   // $(document).on('click', function(event) {
   //   if (!$(event.target).closest('.modal').length) {
@@ -155,7 +182,11 @@ function closeModal() {
   // });
 }
 
-function getCpuBestBuyPrice(sku) {
+function stopModalVideo() {
+  $('.modal-content').find('iframe').prop('src', '');
+}
+
+function getBestBuyPrice(sku, type) {
   const apiKey = bbkey
   const url = bbUrl + sku + '.json' + '?apiKey=' + apiKey;
   fetch(url)
@@ -165,38 +196,105 @@ function getCpuBestBuyPrice(sku) {
       }
       throw new Error(response.statusText);
     })
-    .then(responseJson => renderCpuBestBuyPrice(responseJson))
+    .then(responseJson => renderBestBuyPrice(responseJson, type))
     .catch(err => {
-      $('#js-render-cpu-price').text(`Something went wrong: ${err.message}`);
+      $(`#js-render-${type}-price`).text(`Something went wrong: ${err.message}`);
     });
 }
 
-function getMoboBestBuyPrice(sku) {
-  const apiKey = bbkey
-  const url = bbUrl + sku + '.json' + '?apiKey=' + apiKey;
-  fetch(url)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      throw new Error(response.statusText);
-    })
-    .then(responseJson => renderMoboBestBuyPrice(responseJson))
-    .catch(err => {
-      $('#js-render-mobo-price').text(`Something went wrong: ${err.message}`);
+function renderBestBuyPrice(responseJson, type) {
+  $(`#js-render-${type}-price`).html(`$<span class="js-price">${responseJson.regularPrice}</span>`);
+  let price = $(responseJson.regularPrice);
+}
+
+function getTotalPrice(arr) {
+  const totalPrice = arr.reduce(getSum);
+  roundPrice(totalPrice);
+}
+
+function roundPrice(totalPrice) {
+  const finalPrice = (Math.floor(totalPrice * 100) / 100);
+  renderTotalPrice(finalPrice);
+}
+
+function getSum(total, num) {
+  return total + num;
+}
+
+function renderTotalPrice(finalPrice) {
+  $('#js-render-total-price').html(`<div>Total price: $${finalPrice}</div><div>Price does not include the prices associated with an operating system.</div>`);
+}
+
+function getYoutubeBuild() {
+  const query = 'How to build a PC';
+  const type = 'info';
+  getYoutube(query, type);
+}
+
+function changeFinalPage(priceElements) {
+  let arr = [];
+    $('.js-render').addClass('hidden');
+    $('.js-finish').removeClass('hidden');
+    priceElements.each((idx, element) => {
+      const el = parseFloat($(element).text(), 10);
+      arr.push(el);
     });
+    getTotalPrice(arr);
+    getYoutubeBuild();
 }
 
-function renderCpuBestBuyPrice(responseJson) {
-  $('#js-render-cpu-price').html(`<span>Price in USD: $${responseJson.regularPrice}</span>`);
+function submitBuild(event) {
+  $('#js-pc-parts').submit(function(event) {
+    event.preventDefault();
+    const priceElements = $(this).find('span');
+    if (priceElements.length < 7) {
+      $('#js-error').html('Missing a PC component, please make sure all selections are made.');
+    } else {
+    changeFinalPage(priceElements);
+    startNew();
+    addHover();
+    };
+  });
 }
 
-function renderMoboBestBuyPrice(responseJson) {
-  $('#js-render-mobo-price').html(`<span>Price in USD: $${responseJson.regularPrice}</span>`);
+function addHover() {
+  $('.js-header').addClass('hover');
+}
+
+function removeHover() {
+  $('.js-header').removeClass('hover');
+}
+
+function resetSelection() {
+  Object.keys(STORE).forEach(type => {
+    $(`#js-${type}`)[0].selectedIndex = 0;
+  });
+}
+
+function resetRender() {
+  Object.keys(STORE).forEach(type => {
+    $(`#js-render-${type}`).empty();
+    $(`#js-render-${type}-price`).empty();
+  })
+}
+
+function startNew() {
+  $('.js-header').on('click', function(event) {
+    event.preventDefault();
+    $('.js-finish').addClass('hidden');
+    $('.js-render').removeClass('hidden');
+    resetSelection();
+    resetRender();
+    removeHover();
+    onIntelSelection();
+    onAmdSelection();
+    listenSelection();
+  });
 }
 
 function onStart() {
   startBuild();
+  submitBuild();
 }
 
 $(onStart);
