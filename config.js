b04f05ea53c89d9aabc2332ee2fd3654dd5945b9
Qorvinus <45549391+Qorvinus@users.noreply.github.com>
2019-03-21 16:13:05 -0500

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const ytkey = 'offAIzaSyA33xkrHuTLHRDvibawSxjrekL4GIX66TI';
-
-const ytUrl = 'https://www.googleapis.com/youtube/v3/search';
-
-const bbkey = 'tRN7x3ANSvBOc14AAe9lhXb3';
-
-const bbUrl = 'https://api.bestbuy.com/v1/products/';
